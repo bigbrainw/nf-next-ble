1f34207d0a327dc82a98afb246fa84a34ab03b8b
enkhbold470 <enkhbold470@gmail.com>
2025-07-12 21:24:06 -0700

    Add BleReader component to display list of Bluetooth devices

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
     <AuthWrapper isAuthenticated={authenticated}>
       <div className="min-h-screen bg-gradient-to-br from-slate-50 via-white to-purple-50 dark:from-gray-900 dark:via-gray-950 dark:to-purple-950 dark:text-gray-100">
         <h1 className="text-2xl font-bold dark:text-white">List of bluetooth devices</h1>
+        
         <BleReader />
       </div>
     </AuthWrapper>
